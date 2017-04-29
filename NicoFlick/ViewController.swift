92d301144f68a0c208aee8923962a5a7c5eeb2f9
MIZUSHIKI <water_sheeter@macbook.local>
2017-04-29 20:48:01 +0900

    Initial Commit

---
+++ NicoFlick/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NicoFlick
+//
+//  Created by MIZUSHIKI on 2017/04/29.
+//  Copyright © 2017年 i.MIZUSHIKI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
